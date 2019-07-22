3072b483476385b2f69d143451b38365fa11cd9b
ubi <me@ubidefeo.com>
2019-07-22 11:46:20 +0200

    - astyle on source

--- src/Arduino_DebugUtils.cpp
+++ src/Arduino_DebugUtils.cpp
@@ -93,4 +93,6 @@ void Arduino_DebugUtils::vPrint(char const * fmt, va_list args) {
  ******************************************************************************/
 
 Arduino_DebugUtils Debug;
-void setDebugMessageLevel(int const debug_level) { Debug.setDebugLevel(debug_level); }
+void setDebugMessageLevel(int const debug_level) {
+  Debug.setDebugLevel(debug_level);
+}
